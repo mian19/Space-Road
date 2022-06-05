60acc01bb00193851c6ca4507f069b7b2b742d18
Anton Mishkevich <mian19@protonmail.com>
2022-06-05 22:40:07 +0300

    feat: add pod SwiftLint and fix start warnings

---
+++ Space Road/Space Road/Authentication Screen/AuthenticationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AuthenticationViewController.swift
+//  Space Road
+//
+//  Created by Kyzu on 4.06.22.
+//
+
+import UIKit
+
+class AuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
---
+++ Space Road/Space Road/Main Screen/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Space Road
+//
+//  Created by Kyzu on 4.06.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
